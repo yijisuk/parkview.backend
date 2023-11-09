83a9089b3eda6e6dac9bf5279613022f3e974d15
Yiji <offconstruction@gmail.com>
2023-11-09 16:10:03 +0800

    documentation for view integration apis

--- src/apis/view_integration/favouriteManager.js
+++ src/apis/view_integration/favouriteManager.js
@@ -1,6 +1,15 @@
+// favouriteManager.js: Contains the functions for database operations on favourite locations
+
 import supabase from "../../../config/supabase.js";
 
 
+/**
+ * Retrieves the favourite locations of a user
+ * 
+ * @param {string} userID - User ID
+ * 
+ * @returns {Promise.<Array.<string>>} - Array of favourite location dictionaries
+ */
 export async function getFavouriteLocation(userID) {
     const { data, error } = await supabase
         .from("favorites")
@@ -15,12 +24,28 @@ export async function getFavouriteLocation(userID) {
 }
 
 
+/**
+ * Confirms if a location is a favourite of a user
+ * 
+ * @param {string} userID - User ID
+ * @param {string} locationName - location name to search for
+ * 
+ * @returns {Promise.<boolean>} - true if the location is a favourite, false otherwise
+ */
 export async function checkFavouriteLocation(userID, locationName) {
     const favouriteLocations = await getFavouriteLocation(userID);
     return favouriteLocations.includes(locationName);
 }
 
 
+/**
+ * Adds a location to a user's favourite locations
+ * 
+ * @param {string} userID - User ID
+ * @param {string} location - location name to add
+ * 
+ * @returns {Promise.<string>} - error message if any, empty string otherwise
+ */
 export async function addFavouriteLocation(userID, location) {
     const { error } = await supabase
         .from("favorites")
@@ -30,6 +55,14 @@ export async function addFavouriteLocation(userID, location) {
 }
 
 
+/**
+ * Deletes a location from a user's favourite locations
+ * 
+ * @param {string} userID - User ID
+ * @param {string} location - location name to delete
+ * 
+ * @returns {Promise.<string>} - error message if any, empty string otherwise
+ */
 export async function deleteFavouriteLocation(userID, location) {
     const { error } = await supabase
         .from("favorites")

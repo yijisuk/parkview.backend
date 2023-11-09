9cee45c08c65d7fec8a77aa20891444741ecc8be
Yiji <offconstruction@gmail.com>
2023-11-09 16:19:32 +0800

    documentation for utils and tests

--- src/utils/coordOperations.js
+++ src/utils/coordOperations.js
@@ -1,7 +1,16 @@
+// coordOperations.js: Contains geocoding functions
+
 import { getCoordinatesGMaps } from "../agents/external_apis/googleMaps.js";
 import { getCoordinatesFreeGeocoding } from "../agents/external_apis/alternativeGeocoding.js";
 
 
+/**
+ * Formats a given string address to coordinates
+ * 
+ * @param {string} address - Address to be formatted to coordinates
+ * 
+ * @returns {Promise.<Object>} - Formatted coordinates dictionary containing latitude and longitude
+ */
 export async function formatAddressToCoordinates(address) {
 
     let formattedCoordinates = null;
